a9518e7b853dd4c623bc3be88e5f6515e7d23d48
Aadarsha Dhakal <hello@aadarshadhakal.com.np>
2025-01-24 06:32:50 +0545

    cous

--- playground.py
+++ playground.py
@@ -1,33 +1,49 @@
 # Variables and Data Types
-# We do not specify the data type for the variable
+# Inlike C and C++, in Python we do not specify the data type for the variable
 
-# name = "Harish Aale" #String
-# age = 23 #Int
-# gpa = 3.8 #Float
-# gpas = [3.4, 3.0, 3.8, 4] #List
-# city, province, country = "Kathmandu", "Bagmati", "Nepal"
-# graduated = False #Boolean o or 1
-# achievements = None #Null
-# parents = ("Gita Aale", "Ramu Aale") #Tuple Immutable List
+name = "Harish Aale" #String
+age = 23 #Int
+gpa = 3.8 #Float
+gpas = [3.4, 3.0, 3.8, 4] #List, by default mutable.
+city, province, country = "Kathmandu", "Bagmati", "Nepal"
+graduated = False #Boolean o or 1
+achievements = None #Null
+parents = ("Gita Aale", "Ramu Aale") #Tuple aka Immutable List
 
 
 # Since we have not defined types for the variable
-# Lets check type inference in action
-# type(name)
-# type(age)
-# type(gpa)
+# Let's see type inference in action
+type(name)
+type(age)
+type(gpa)
 
-# No of semester Harish Aale has passed
-# len(gpas)
+# No of semester Harish Aale has passed ?
+# len() function prints the length of a list
+len(gpas)
+
 # What is the gpa obtained by Harish Aale in 3rd semester?
-# print(gpas[2])
+print(gpas[2])
+
 # What is the gpa obtained by Harish Aale upto 3rd semester?
-# print(gpas[:3]) #or
-# print(gpas[0:3])
+
+print(gpas[:3]) #or
+print(gpas[0:3])
+
 # To Print whole list
-# gpas[:] #or
-# gpas[0:len(gpas)]
-# gpas[::]
+gpas[:] #or
+gpas[0:len(gpas)]
+gpas[::]
+
+# Print odd semester marks of Harish Aale
+gpas[0:4:2] # Prints from zeroth index to third index with step size 2. 
+
+# Modify Items in a List
+gpas[0] = 3.0
+gpas[-1] = 4
+
+# We cannot modify a tuple. Why? Because tuple is immutable.
+gpas[0] = "Sita Aale" # throws error
+
 
 # Other than this
 # Complex Numeric Literal
@@ -71,6 +87,8 @@ del dictionary["city"]
 # else:
 #     print("Invalid Gender")
 
+## Classwork GPA to Grade Converter
+
 
 #### Loop
 
@@ -102,9 +120,19 @@ del dictionary["city"]
 
 
 
-# What are the gpas optained by Harish Aale in odd semesters?
+# What are the gpas optained by Harish Aale in even semesters?
 gpas = [3.4, 3.0, 3.8, 4] 
 for index in range(len(gpas)):
     if index % 2 == 0:
         continue
     print(gpas[index])
+
+
+# Calculate CGPA of harish aale?
+total_gpa = sum(gpas)
+num_semesters = len(gpas)
+cgpa = total_gpa / num_semesters
+print(cgpa)
+
+
+#Functions
