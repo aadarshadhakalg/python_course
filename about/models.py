7f73b25a7ec4393cac15204a1671ef08d5523016
Kasmi Thapa <kasmithapa00@gmail.com>
2025-03-13 22:02:26 +0545

    hee

--- about/models.py
+++ about/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class MyInformation(models.Model):
+    first_name = models.CharField(max_length=255,blank=False, null=False)
+    last_name = models.CharField(max_length=255,blank=False, null=False)
+    bio = models.CharField(max_length=255, blank=False, null=False)
+    photo = models.ImageField(upload_to='photos/')
+
+    def __str__(self):
+        return self.first_name
+    
+    
+
+class SocialMedia(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    username = models.CharField(max_length=20,blank=False,null=False)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
+    
--- about/views.py
+++ about/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . import models
 
 def index(request):
+    aboutme = models.MyInformation.objects.first()
+    socials = models.SocialMedia.objects.all()
     context = {
-        "name": "Aadarsha Dhakal",
-        "bio": "Google Me!!",
-        "social": {
-            "facebook":"aadarshadhakalg",
-            "instagram": "aadarshadhakalg",
-            "twitter": "aadarshadhakalg",
-            "github": "aadarshadhakalg",
-        }
+        "name": f'{aboutme.first_name} {aboutme.last_name}',
+        "bio": aboutme.bio,
+        "social": socials,
+        "photo": aboutme.photo.url,
     }
     return render(request,"index.html", context=context)
